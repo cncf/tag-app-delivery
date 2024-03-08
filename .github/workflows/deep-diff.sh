6617ccc1647f5484a11fe96098843ac21811907a
Hiroshi Hayakawa <hhiroshell@gmail.com>
2024-03-08 17:15:49 +0900

    Make the GitHub Action work on PR from forked repository.
    Signed-off-by: Hiroshi Hayakawa <hhiroshell@gmail.com>

--- .github/workflows/deep-diff.sh
+++ .github/workflows/deep-diff.sh
@@ -4,26 +4,17 @@
 
 set -e
 
-if [ "$#" -ne 1 ] && [ "$#" -ne 3 ]; then
-    echo "The number of arguments must be either 1 or 3."
-    echo "Usage: ./list-targets.sh [path] <[commit] [commit]>"
+if [ "$#" -ne 3 ]; then
+    echo "The number of arguments must be 3." >&2
+    echo "Usage: ./list-targets.sh [path] [commit-before] [commit-after]" >&2
     exit 1
 fi
 
-if [ "$#" -eq 1 ]; then
-  git diff --name-only "$1"
-else
-  git diff --name-only "$2" "$3" "$1"
-fi
+git diff --name-only "$2" "$3" "$1"
 
 # Differences in targets of symbolic links.
 symlinks=$(find "$1" -type l)
 for l in ${symlinks}; do
-  linktarget=$(readlink -f "${l}")
-
-  if [ "$#" -eq 1 ]; then
-    git diff --name-only "$(realpath --relative-to=. "${linktarget}")"
-  else
+    linktarget=$(readlink -f "${l}")
     git diff --name-only "$2" "$3" "$(realpath --relative-to=. "${linktarget}")"
-  fi
 done
